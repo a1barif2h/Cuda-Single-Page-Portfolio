d97a30707a1b246dbf252cff2374c55453c28f3a
a1barif2h <a1barif2h@gmail.com>
2020-06-28 10:54:31 +0600

    Cuda singel page portfolio

---
+++ resources/js/main.js
@@ -0,0 +1,34 @@
+$(document).ready(function(){
+    
+    
+    //STICKY MENU
+    
+   // $(".js--services-section").waypoint(function(direction){
+   //     if(direction == "down"){
+    //       $("nav").addclass("sticky");
+     //      } else {
+       //     $("nav").removeclass("sticky");
+      //     }
+        
+  //  });
+    
+    //W3SCHOOL
+    window.onscroll = function() {myFunction()};
+
+var navbar = document.getElementById("navbar");
+var sticky = navbar.offsetTop;
+
+function myFunction() {
+  if (window.pageYOffset >= sticky) {
+    navbar.classList.add("sticky")
+  } else {
+    navbar.classList.remove("sticky");
+  }
+}
+    
+    
+    
+    
+    //MIXITUP(PROTFOLIO)
+    var mixer = mixitup(".container")
+});
